8c8f8e8a4ab586ca23312d94c8cc376e53d4c8ef
selim reza swadhin <selimrezaswadhim@gmail.com>
2020-05-31 15:01:38 +0600

    crud ajax and php

---
+++ crud-app.js
@@ -0,0 +1,68 @@
+
+$( document ).ready(function() {
+    getAllData();
+    console.log( "ready!" );
+});
+$("form#crudAppForm").on("submit",function (e) {
+    e.preventDefault();
+    let name = $("#name").val();
+    let email = $("#email").val();
+    let contact = $("#contact").val();
+    let editId = $("#editId").val();
+    if (name == ""){
+        alert("Please enter name");
+        $("#name").focus();
+    }else if (email == "") {
+        alert("Please enter email");
+        $("#email").focus();
+    }else if (contact == "") {
+        alert("Please enter contact");
+        $("#contact").focus();
+    }else{
+        $("#buttonLabel").html("Saving...");
+        $("#spinnerLoader").show('fast');
+        $.post("crud-script.php",{
+            crudOperation:"saveData",
+            name:name,
+            email:email,
+            contact:contact,
+            editId:editId,
+        },function (response) {
+            if (response == "saved") {
+                $("#buttonLabel").html("Save");
+                $("#spinnerLoader").hide('fast');
+                $("#myModal").modal('hide');
+                $("form#crudAppForm").each(function () {
+                    this.reset();
+                });
+                getAllData();
+            }
+        });
+    }
+});
+
+function getAllData() {
+    $.post("crud-script.php",{crudOperation:"getData"},function (allData) {
+        $("#crudData").html(allData);
+    });
+}
+
+function editData(editId,name,email,contact) {
+    $("#editId").val(editId);
+    $("#name").val(name);
+    $("#email").val(email);
+    $("#contact").val(contact);
+    $("#myModal").modal('show');
+}
+
+function deleteData(deleteId) {
+    if(confirm("Are you sure to delete this ?")){
+        $('#deleteSpinner'+deleteId).show('fast');
+        $.post("crud-script.php",{crudOperation:"deleteData",deleteId:deleteId},function (response) {
+            if (response == "deleted") {
+                $('#deleteSpinner'+deleteId).hide('fast');
+                getAllData();
+            }
+        });
+    }
+}
